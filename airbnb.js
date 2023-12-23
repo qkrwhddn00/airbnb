245998c6655f36d545613c306bfec26b3647890b
qkrwhddn00 <qkrwhddn1217@naver.com>
2023-12-23 17:00:14 +0900

    second upload

--- airbnb.js
+++ airbnb.js
@@ -1,20 +1,207 @@
-// CORS Anywhere 프록시를 통해 실제 room.json 파일에 대한 URL을 호출합니다.
-fetch('https://cors-anywhere.herokuapp.com/실제-room-json-파일-URL')
-  .then(response => response.json())
-  .then(data => {
-    var rooms = data.rooms;
+var roomsData = {
+    "rooms": [
+        {
+            "id": "0",
+            "image": "https://a0.muscache.com/im/pictures/miso/Hosting-926638381418918258/original/ff037c2c-2d78-4625-a8e7-34efbf241544.jpeg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★4.86",
+            "info": "산 및 정원 전망",
+            "date": "1월 18일~23일",
+            "price": "₩194000/박"
+        },
+        {
+            "id": "1",
+            "image": "https://a0.muscache.com/im/pictures/c044dc6f-ea23-4839-9d3e-d0a9b6d0ba03.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★NEW",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "2",
+            "image": "https://a0.muscache.com/im/pictures/e13ac058-ce69-4c48-9a05-f2f71667af93.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★4.94",
+            "info": "호수 및 강전만",
+            "date": "1월 15일~20일",
+            "price": "₩178024/박"
+        },
+        {
+            "id": "3",
+            "image": "https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTAxODcwNjE1ODExODg2NDg3NA%3D%3D/original/89ec0f90-9d03-4819-b84f-9eb2cbfcb67a.png?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "4",
+            "image": "https://a0.muscache.com/im/pictures/e2bcb474-9938-47b0-a417-c4a02fd0532a.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "5",
+            "image": "https://a0.muscache.com/im/pictures/043ada40-cfd5-442c-bd79-1cc72542b4d7.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★4.86",
+            "info": "산 및 정원 전망",
+            "date": "1월 18일~23일",
+            "price": "₩194000/박"
+        },
+        {
+            "id": "6",
+            "image": "https://a0.muscache.com/im/pictures/hosting/Hosting-1005524787030880312/original/ba9611c6-30bb-4708-9dff-cce62c927c25.jpeg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "7",
+            "image": "https://a0.muscache.com/im/pictures/34fbd77d-293f-4425-9ecb-b835378e3213.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "8",
+            "image": "https://a0.muscache.com/im/pictures/miso/Hosting-810568922961743066/original/cca44cc2-1f4e-40bb-bce5-cbe95dee965c.jpeg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "9",
+            "image": "https://a0.muscache.com/im/pictures/1d82d177-68ee-4216-8d31-26e182f43738.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "10",
+            "image": "https://a0.muscache.com/im/pictures/miso/Hosting-800491396337233542/original/d49d5429-2d3f-4088-aa3e-27bcfd685692.jpeg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★4.86",
+            "info": "산 및 정원 전망",
+            "date": "1월 18일~23일",
+            "price": "₩194000/박"
+        },
+        {
+            "id": "11",
+            "image": "https://a0.muscache.com/im/pictures/airflow/Hosting-962132618872003713/original/8180d863-5c0f-43ab-969a-216316221986.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "12",
+            "image": "https://a0.muscache.com/im/pictures/2cd5348f-a30f-4564-9cec-61b612a273bd.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "13",
+            "image": "https://a0.muscache.com/im/pictures/3a69fb6f-03ad-4b37-adfd-26881c867647.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "14",
+            "image": "https://a0.muscache.com/im/pictures/miso/Hosting-876528672826850380/original/dd1eaead-1966-4815-84d2-d057a74c8a81.jpeg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "15",
+            "image": "https://a0.muscache.com/im/pictures/miso/Hosting-780076872361737192/original/8186cc14-e1cd-42b6-b708-3de96b1cf593.jpeg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★4.86",
+            "info": "산 및 정원 전망",
+            "date": "1월 18일~23일",
+            "price": "₩194000/박"
+        },
+        {
+            "id": "16",
+            "image": "https://a0.muscache.com/im/pictures/hosting/Hosting-1028135166340028664/original/027c62e3-8f22-44bb-a3f9-b75b8f57860f.jpeg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "17",
+            "image": "https://a0.muscache.com/im/pictures/6c270f2c-f211-417a-9073-a97d6a307c54.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "18",
+            "image": "https://a0.muscache.com/im/pictures/c044dc6f-ea23-4839-9d3e-d0a9b6d0ba03.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+        {
+            "id": "19",
+            "image": "https://a0.muscache.com/im/pictures/cb1c8322-bb02-4722-bc09-dfc55ced74f7.jpg?im_w=720",
+            "name": "한국 서울",
+            "grade": "★new",
+            "info": "강변 전망",
+            "date": "1월 1일~6일",
+            "price": "₩170948/박"
+        },
+    ]
+};
 
-    rooms.forEach(function(room, index) {
-      $('.room-info').append(`
-        <div class="room_id" draggable="true" data-id="${room.id}">
-          <img src="${room.image}" class="room_img">
-          <p>${room.price}</p>
-          <p>${room.name}</p>
-          <p>${room.info}</p>
+
+var rooms = roomsData.rooms;
+var roomInfoElement = document.querySelector('.room-info');
+
+rooms.forEach(function(room) {
+    var roomElement = document.createElement('div');
+    roomElement.classList.add('room-item'); 
+    roomElement.classList.add('name_grade');
+    roomElement.classList.add('room_price');
+    roomElement.classList.add('room_img');
+    roomElement.innerHTML = `
+        <img src="${room.image}" class="room_img">
+        <div class="name_grade">  
+          <p>${room.name} </p>
+          <p>${room.grade}</p>
         </div>
-      `);
-    });
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+        <p>${room.info}</p>
+        <p>${room.date}</p>
+        <p class="room_price">${room.price}</p>
+    `;
+    roomInfoElement.appendChild(roomElement);
+});
